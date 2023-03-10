ebde822dd9b449f2acd07531f615e73bc4b7b15f
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-10 18:32:26 -0500

    created server connection in server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./config/connection');
+// const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// app.use(routes);
+
+app.listen(PORT, () => {console.log('Connected to the server!')});
