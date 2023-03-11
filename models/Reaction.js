7fbee25d36e560ffc15e6246c17d5497ed8fb7cb
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-11 02:31:03 -0500

    created reactionSchema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,26 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, default: mongoose } = require('mongoose');
+
+const reactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Schema.Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: [280, 'Must be less than 280 characters, got {VALUE}']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function (timestamp) {
+                return Date(timestamp).toLocaleDateString();
+            }
+        }
+    }
+)
