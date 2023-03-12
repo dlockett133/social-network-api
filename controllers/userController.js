8d0a3a3ce4d69358812001e1ea7f94b81d8bcd42
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-11 20:38:18 -0500

    created route to get one user by _id

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,47 +19,5 @@ module.exports = {
                 console.log(err);
                 res.status(500).json(err);
             });
-    },
-
-    createUser(req,res) {
-        User.create(req.body)
-            .then((user) => res.json(user))
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-    },
-
-    updateUser(req,res) {
-        User.findByIdAndUpdate(
-            req.params.userId, 
-            req.params.body,
-            {
-                new: true,
-                runValidators: true
-            })
-            .populate('thoughts')
-            .populate('friends')
-            .then((user) => {
-                !user 
-                    ? res.status(400).json({message: 'Found no user with this ID'})
-                    : res.json(user);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-    },
-    updateUser(req,res) {
-        User.findByIdAndDelete(req.params.userId)
-            .then((user) => {
-                !user 
-                    ? res.status(400).json({message: 'Found no user with this ID'})
-                    : res.json({message: 'User deleted successfully'});
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            });
     }
 }
